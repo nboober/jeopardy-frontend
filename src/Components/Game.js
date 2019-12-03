7bbaf555db2fd0807dc6e7c14cd64cc4d88e92bd
Nick <nboober@terpmail.umd.edu>
2019-12-03 09:38:00 -0500

    got some data to render to the page

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import Column from './Column'
+import Row from './Row'
 
 let Game = (props) => {
+    console.log(props)
     return (
         <div>
-        <h3>game cards here
-        </h3>
-        <Column 
-        cat1={props.cat1} 
-        cat2={props.cat2} 
-        cat3={props.cat3} 
-        cat4={props.cat4}
-        cat5={props.cat5}
-        />
+            <h3>game cards here
+            </h3>
+            <table>
+                <tr>
+                    <th>{props.catTitles[0]}</th>
+                    <th>{props.catTitles[1]}</th>
+                    <th>{props.catTitles[2]}</th>
+                    <th>{props.catTitles[3]}</th>
+                    <th>{props.catTitles[4]}</th>
+                </tr>
+
+            {props.val200.map(question => {
+
+                return <tr><Row key={question.category.id} questionData={question} /></tr>})
+
+            }
+            {props.val400.map(question => {
+
+                return <tr><Row key={question.category.id} questionData={question} /></tr>})
+
+            }
+            {props.val600.map(question => {
+
+                return <tr><Row key={question.category.id} questionData={question} /></tr>})
+
+            }
+            {props.val800.map(question => {
+
+                return <tr><Row key={question.category.id} questionData={question} /></tr>})
+
+            }
+            {props.val1000.map(question => {
+
+                return <tr><Row key={question.category.id} questionData={question} /></tr>})
+
+            }
+
+            </table>
+
         </div>
     )
 }
---
+++ src/Components/Row.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let Row = (props) => {
+    // console.log(props.questionData)
+    return (
+        <>
+                <td>{props.questionData.question}</td>
+        </>
+        
+            
+    )
+}
+
+export default Row
