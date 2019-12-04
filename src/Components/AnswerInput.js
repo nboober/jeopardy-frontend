9cd3f4ddd7d311542b267af0105bf5ec8c4988d2
Nick <nboober@terpmail.umd.edu>
2019-12-04 12:56:27 -0500

    added styling to forms

--- src/Components/AnswerInput.js
+++ src/Components/AnswerInput.js
@@ -5,8 +5,10 @@ let AnswerInput = (props) => {
     return (
         <div>
             <form onSubmit={props.submitAnswer}>
-                <input type='text' placeholder='Your Answer Here' value={props.userAnswer} onChange={props.answer}/>
-                <input type='submit' value='Submit'/>
+            <div className="row d-flex justify-content-center ">
+                <input className="form-control col-sm-5" type='text' placeholder='Your Answer Here' value={props.userAnswer} onChange={props.answer}/>
+                <input className="btn btn-warning col-sm-2" type='submit' value='Submit'/>
+            </div>
             </form>
         </div>
     )
--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -3,12 +3,14 @@ import React from 'react';
 let LoginForm = (props) => {
     
     return (
-        <div style={{textAlign: "center", backgroundImage: "linear-gradient(to left bottom, #051937, #331a4c, #66004b, #900033, #a40101)", height: "100vh", color: "white"}}>
+        <div  style={{textAlign: "center", backgroundImage: "linear-gradient(to left bottom, #051937, #331a4c, #66004b, #900033, #a40101)", height: "100vh", color: "white"}}>
             <h2 className="title-font">Ready to Play Jeopardy?</h2>
             <form onSubmit={props.login}>
-                <input type='text' placeholder='Enter Username' value={props.username} name='username' onChange={props.collect}/>
-                <input type='password' placeholder='Enter Password'value={props.password} name='password'onChange={props.collect}/>
-                <input type='submit' value='Login'/>
+                <div className="row d-flex justify-content-center ">
+                    <input className="form-control col-sm-5" type='text' placeholder='Enter Username' value={props.username} name='username' onChange={props.collect}/>
+                    <input className="form-control col-sm-5" type='password' placeholder='Enter Password'value={props.password} name='password'onChange={props.collect}/>
+                    <input className="btn btn-warning col-sm-3" type='submit' value='Login'/>
+                </div>
             </form>
         </div>
     )
