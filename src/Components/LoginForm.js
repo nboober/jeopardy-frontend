5587e0c48e385c6ff62f63390229b6ba7f0f4c1b
Nick <nboober@terpmail.umd.edu>
2019-12-04 13:29:28 -0500

    centered login page

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -4,14 +4,16 @@ let LoginForm = (props) => {
     
     return (
         <div  style={{textAlign: "center", backgroundImage: "linear-gradient(to left bottom, #051937, #331a4c, #66004b, #900033, #a40101)", height: "100vh", color: "white"}}>
+           <div style={{paddingTop: "15%"}}>
             <h2 className="title-font">Ready to Play Jeopardy?</h2>
             <form onSubmit={props.login}>
-                <div className="row d-flex justify-content-center">
+                <div className="row d-flex justify-content-center" >
                     <input className="form-control col-sm-5" type='text' placeholder='Enter Username' value={props.username} name='username' onChange={props.collect}/>
                     <input className="form-control col-sm-5" type='password' placeholder='Enter Password'value={props.password} name='password'onChange={props.collect}/>
                     <input className="btn btn-warning col-sm-3" type='submit' value='Login'/>
                 </div>
             </form>
+           </div>
         </div>
     )
 }
