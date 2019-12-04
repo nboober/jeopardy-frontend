d791e28b2028cad1afe24307d4ac17a43dd5acff
Nick <nboober@terpmail.umd.edu>
2019-12-04 13:01:34 -0500

    trying to vertically align

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -6,7 +6,7 @@ let LoginForm = (props) => {
         <div  style={{textAlign: "center", backgroundImage: "linear-gradient(to left bottom, #051937, #331a4c, #66004b, #900033, #a40101)", height: "100vh", color: "white"}}>
             <h2 className="title-font">Ready to Play Jeopardy?</h2>
             <form onSubmit={props.login}>
-                <div className="row d-flex justify-content-center ">
+                <div className="row d-flex justify-content-center">
                     <input className="form-control col-sm-5" type='text' placeholder='Enter Username' value={props.username} name='username' onChange={props.collect}/>
                     <input className="form-control col-sm-5" type='password' placeholder='Enter Password'value={props.password} name='password'onChange={props.collect}/>
                     <input className="btn btn-warning col-sm-3" type='submit' value='Login'/>
